9d840b5af282d640a8e86cb453a9e21d084743ef
Grigor <grigorsafaryan36@gmai.com>
2022-06-23 21:27:47 -0700

    Models_migrations

---
+++ app/Models/Posts.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function tags(){
+        return $this->hasMany(Tags::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comments::class);
+    }
+}
